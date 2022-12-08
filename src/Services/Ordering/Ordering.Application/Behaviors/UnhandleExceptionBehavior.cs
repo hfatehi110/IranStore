3438fa1bdecbd601dab53699922e5e809058cd09
hfatehi110 <hfatehi110@outlook.com>
2022-12-08 22:08:31 +0330

    نغییرات

---
+++ src/Services/Ordering/Ordering.Application/Behaviors/UnhandleExceptionBehavior.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviors
+{
+    internal class UnhandleExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : MediatR.IRequest<TResponse>
+    {
+        private readonly ILogger< TRequest> _logger;
+
+        public UnhandleExceptionBehavior(ILogger<TRequest> request)
+        {
+            _logger = request;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError( ex,"Application Request : Unhandled Exception For Request {Name} {@Request}",requestName,request);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Ordering.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest,TResponse> where TRequest : MediatR.IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                // این خط تمام ولیدیشن هایی که وجود دارد را چک میکند
+                var validationResult = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failure = validationResult.SelectMany(r => r.Errors).Where(f => f != null).ToList();  
+
+                if(failure.Count > 0) 
+                    throw new ValidationException(failure);
+
+
+            }
+            return await next();
+        }
+    }
+}
